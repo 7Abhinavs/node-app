32d4dbf53e959cc964591ccf54e06d0f47b74fa5
7Abhinavs <abhinav.20m0141045@abes.ac.in>
2022-05-25 10:17:29 +0530

    official changes 3

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with Hello from Abhinav Chauhan This is an assignment made on aws,nginx,jenkins and git Thank You', function(done) {
+ it('respond with Hello from Abhinav Chauhan This is an assignment made on aws,nginx,jenkins and git Thank You for providing this assignmnet ', function(done) {
   //navigate to root and check the response is "hello world"
-  request(app).get('/').expect('Hello from Abhinav Chauhan This is an assignment made on aws,nginx,jenkins and git Thank You', done);
+  request(app).get('/').expect('Hello from Abhinav Chauhan This is an assignment made on aws,nginx,jenkins and git Thank You for providing this assignmnet', done);
  });
 });

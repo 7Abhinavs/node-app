7acdf8659f0016c19dc77ae4a0c83ffd5b4439b1
7Abhinavs <abhinav.20m0141045@abes.ac.in>
2022-05-25 10:15:50 +0530

    official changes 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
  * Respond with "hello world" for requests that hit our root "/"
  */
 app.get('/', function (req, res) {
-res.send('Hello from Abhinav Chauhan This is an assignment made on aws,nginx,jenkins and git Thank You');
+res.send('Hello from Abhinav Chauhan This is an assignment made on aws,nginx,jenkins and git Thank You for providing this assignmnet');
 })
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
